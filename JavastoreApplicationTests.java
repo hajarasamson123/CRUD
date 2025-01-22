b9a656fdf011c2d15a3344271d17f72c66d76cb6
hajarasamson123 <132559216+hajarasamson123@users.noreply.github.com>
2025-01-22 08:44:11 +0100

    Add files via upload

---
+++ JavastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastore.javastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
